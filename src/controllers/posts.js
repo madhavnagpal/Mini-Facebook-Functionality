2410f866197f0f1c830f4f85a979bdda4234ccad
Madhav Nagpal <mr.madhavnagpal@gmail.com>
2020-05-11 22:04:37 +0530

    post made simple

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,27 +1,21 @@
-const { Users, Posts } = require("../../db/index");
+const { Users, Posts } = require("../db/index");
 
 async function createPost(user_id, post_title, post_body) {
-  let post;
-  Users.findOne({
-    where: { user_id },
-  })
-    .then((user) => {
-      Posts.create({
+  try {
+    let user = await Users.findOne({
+      where: { user_id },
+    });
+    if (user) {
+      let post = await Posts.create({
         post_title,
         post_body,
         userUserId: user_id,
-      })
-        .then((p) => {
-          post = p;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    })
-    .catch((err) => {
-      post = null;
-    });
-  return post;
+      });
+      return post;
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 module.exports = {
--- src/routes/posts/index.js
+++ src/routes/posts/index.js
@@ -1,6 +1,6 @@
 const { Users, Posts, Comments } = require("../../db/index");
 const route = require("express").Router();
-// const { createPost } = require("../../controllers/posts");
+const { createPost } = require("../../controllers/posts");
 
 // get all posts
 route.get("/", async function (req, res) {
@@ -21,8 +21,6 @@ route.post("/", async function (req, res) {
     res.status(403).send("Bad Request , it must have user_id,title,body");
   } else {
     const post = await createPost(id, title, body);
-    console.log(post);
-    console.log("=======");
     if (post) {
       res.status(201).send(post);
     } else {
@@ -32,34 +30,3 @@ route.post("/", async function (req, res) {
 });
 
 module.exports = route;
-
-async function createPost(user_id, post_title, post_body) {
-  let post;
-
-  let user = await Users.findOne({
-    where: { user_id },
-  }).catch((err) => {
-    console.log(err);
-  });
-
-  await (async function () {
-    if (user) {
-      let p = await Posts.create({
-        post_title,
-        post_body,
-        userUserId: user_id,
-      });
-
-      await (function () {
-        if (p) {
-          post = p;
-        } else {
-          post = new Error("Post Not Created");
-        }
-      })();
-    } else {
-      post = null;
-    }
-  })();
-  return post;
-}
