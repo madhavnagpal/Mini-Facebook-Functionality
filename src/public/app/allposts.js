4dba108f928e6f1c739c9a7a2757e799703099ab
Madhav Nagpal <mr.madhavnagpal@gmail.com>
2020-05-17 19:16:45 +0530

    comments working bug fixed

--- src/public/app/allposts.js
+++ src/public/app/allposts.js
@@ -1,48 +1,53 @@
 function getAllPosts() {
+  // ajax request POST
   $.get("/posts", {}, (posts) => {
     let list = $("#allPosts");
     list.empty();
+    //iterating on all posts
     for (let post of posts) {
-      let item = $(`
-      <div class="col-md-4 col-sm-6" >
-      <div class="card m-2 " style="background-color:lavender;">
-        <div class="card-body">
-          <h5 class="card-title">${post.post_title}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">${post.user.user_name}</h6>
-          <p class="card-text">
-            ${post.post_body.substr(0, 200)}
-            <a href="#">...read more</a>
-          </p>
-          <input type="text" placeholder="add suggestions">
-          <button class="card-link btnComment">Comment</button>
-          <ul class="comment" id=${post.post_id}> </ul> 
-        </div>
-      </div>
-       </div>`);
-
-      // let commentBox = $("<ul></ul>");
-      // for (let comment of post.comments) {
-      //   commentBox.append($("<li>").text(comment.comment_body));
-      // }
-      // item.append(commentBox);
-      // list.append(item);
-      let commentBox = $(`#${post.post_id}`);
-
-      // console.log(commentBox, post.post_id);
+      let count = 1;
+      let item = $(
+        `
+                  <div class="col-md-4 col-sm-6" >
+                    <div class="card m-2 " style="background-color:lavender;">
+                      <div class="card-body">
+                        <h5 class="card-title">${post.post_title}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${
+                          post.user.user_name
+                        }</h6>
+                        <p class="card-text">
+                          ${post.post_body.substr(0, 200)}
+                          <a href="#">...read more</a>
+                        </p>
+                        <input type="text" placeholder="add suggestions" class="newComment">
+                        <button class="card-link btnComment">Comment</button>  
+                      </div>
+                      <ul class="comment ${post.post_id}">
+                      </ul> 
+                    </div>
+                  </div>`
+      );
+      let commentBox = item.find(".comment");
       for (let comment of post.comments) {
-        commentBox.append(
-          $("<li>").text(comment.comment_body).css({
-            color: "black",
-          })
-        );
-        console.log(commentBox);
+        commentBox.append($("<li></li>").text(comment.comment_body));
       }
-      // item.append(commentBox);
-      list.append(item);
 
-      $(".btnComment").on("click", (e) => {
-        console.log(post);
+      list.append(item);
+      item.find(".btnComment").on("click", () => {
+        $.post(
+          "/comments",
+          {
+            post_id: post.post_id,
+            comment_body: item.find(".newComment").val(),
+            user_id: JSON.parse(window.localStorage.user).user_id,
+          },
+          (comment) => {
+            $("#content").load(`/components/allpost.html`);
+          }
+        );
       });
     }
+    //loop ends
   });
+  //get request ends
 }
--- src/public/app/design.js
+++ src/public/app/design.js
@@ -1,9 +1,3 @@
 $("#navbar").load("./components/navbar.html");
 $("#footer").load("./components/footer.html");
 $("#content").load(`/components/allpost.html`);
-
-function addComment(post) {
-  console.log("hello", post);
-}
-// const btns = document.getElementsByClassName("btnComment");
-// btns.addEventListener("click", addComment);
